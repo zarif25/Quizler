699d76687ab6ada6fde1f61314dd29836afd97c1
zarif25 <2502.zarif@gmail.com>
2022-05-24 20:06:46 +0600

    color constants

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class KColors {
+  static const Color primary = Color(0xFF8D6E63);
+  static const Color pureBlack = Colors.black;
+  static const Color pureWhite = Colors.white;
+  static const Color correct = Color(0xFF4CAF50);
+  static const Color incorrect = Color(0xFFF44336);
+  static const Color mattBlack = Color(0xFF333333);
+  static const Color gray = Color(0xFF8A8A8A);
+}
